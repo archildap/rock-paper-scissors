00abbff8c2edb06293804d73b71c9e43806a470a
Archil Dapkviashvili <archil.dapkviashvili@gmail.com>
2022-10-18 04:32:30 +0400

    replace console log game with buttons and text

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const roundResult = document.querySelector('.result p');
+const finalResult = document.querySelector('.final-result')
+const buttons = document.querySelectorAll('.btn');
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let array = ["Rock", "Paper", "Scissor"];
@@ -12,62 +17,52 @@ function getComputerChoice() {
     }
 }
 
-let result;
-
-function playRound(playerSelection, computerSelection) {    
-    if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "Paper") {
-        result = "You lost! Paper beats Rock";
-        return "Computer";
+function checkForWinner (playerScore, computerScore) {
+    if(playerScore === 5){
+        finalResult.textContent = "CONGRATS! YOU WON THE GAME";
+    } else if (computerScore === 5) {
+        finalResult.textContent = "You Lost! Good Luck Next Time";
+    }
+}
+ 
+function playRound(playerSelection, computerSelection) {
+    if(playerScore === 5 || computerScore === 5) {
+        return;    
+    } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "Paper") {
+        computerScore++
+        roundResult.textContent = `You lost! Paper beats Rock, SCORE|| Player: ${playerScore}  Computer: ${computerScore}`;
+        return "Computer";        
     } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection === "Scissor") {
-        result = "You Won! Rock beats Scissors";
+        playerScore++
+        roundResult.textContent = `You Won! Rock beats Scissors, SCORE|| Player: ${playerScore}  Computer: ${computerScore}`;        
         return "Player";
     } else if (playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
+        roundResult.textContent = `Draw!, SCORE|| Player: ${playerScore}  Computer: ${computerScore}`;
         return "Draw";
     } else if (playerSelection.toUpperCase() === "SCISSOR" && computerSelection === "Rock") {
-        result = "You lost! Rock beats Scissors";
+        computerScore++
+        roundResult.textContent = `You lost! Rock beats Scissors, SCORE|| Player: ${playerScore}  Computer: ${computerScore}`;       
         return "Computer";
     } else if (playerSelection.toUpperCase() === "SCISSOR" && computerSelection === "Paper") {
-        result = "You Won! Scissors beat Paper";
+        playerScore++
+        roundResult.textContent = `You Won! Scissors beat Paper, SCORE|| Player: ${playerScore}  Computer: ${computerScore}`;         
         return "Player";
     } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "Scissor") {
-        result = "You lost! Scissors beat Paper";
+        computerScore++
+        roundResult.textContent = `You lost! Scissors beat Paper, SCORE|| Player: ${playerScore}  Computer: ${computerScore}`; ;        
         return "Computer";
     } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection === "Rock") {
-        result = "You Won! Paper beats Rock";
+        playerScore++
+        roundResult.textContent = `You Won! Paper beats Rock, SCORE|| Player: ${playerScore}  Computer: ${computerScore}`;        
         return "Player";
     } 
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-        
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Plase Choose Rock, Paper or Scissor");
-        let computerSelection = getComputerChoice();
 
-        if (playRound(playerSelection, computerSelection) === "Player") {
-            playerScore++;
-            console.log(result + " Player score: " + playerScore + " Computer score: " + computerScore);
-
-        } else if (playRound(playerSelection, computerSelection) === "Computer") {
-
-            computerScore++;
-            console.log(result + " Player score: " + playerScore + " Computer score: " + computerScore);
-
-        } else if (playRound(playerSelection, computerSelection) === "Draw") {
-            i--;
-
-            console.log("Draw! Player score: " + playerScore + " Computer score: " + computerScore);
-        }
-    }
-
-    if (playerScore > computerScore) {
-        console.log("Congrats! You WON THE GAME")
-    } else if (playerScore < computerScore) {
-        console.log("You Lost! Good luck next time")
-    } else console.log("The Game Is A Draw!")
-}
-
-game();
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {  
+    playRound(e.target.textContent, getComputerChoice());
+    checkForWinner(playerScore, computerScore); 
+    });
+});
 
